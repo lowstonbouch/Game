7fef5c45b6f5abc9fedea3ae7c374693ff5b1a8b
lowstonbouch <lowstonbouch@gmail.com>
2017-12-13 01:18:08 +0300

    a

--- src/images/index.js
+++ src/images/index.js
@@ -3,7 +3,7 @@ const images = {
     platform1: require('./platform1.png'),
     grass_main: require('./grass_main.png'),
     generic_deathtiles: require('./generic_deathtiles.png'),
-    player: require('./player.png'),
+    player: require('./dist/player.png'),
     cursor: require('./cursor.png'),
     hook: require('./hook.png'),
     flag_start: require('./flagStart.png'),
