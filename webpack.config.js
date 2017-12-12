e498065d0d5282e3c3d924659ae211afc60e3c9b
lowstonbouch <lowstonbouch@gmail.com>
2017-12-13 01:36:45 +0300

    bild

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,6 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'game.js',
-        publicPath: 'src',
     },
     resolve: {
         extensions: ['.ts', '.js'],
